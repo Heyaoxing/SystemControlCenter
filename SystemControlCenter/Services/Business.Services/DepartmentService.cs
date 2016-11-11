ff0a969c98296c250fc445fa314d2096003aa8a9
730530507 <730530507@qq.com>
2016-11-11 18:11:12 +0800

    dw

---
+++ SystemControlCenter/Services/Business.Services/DepartmentService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Entities;
+using Data.Repositories;
+using IBusiness.Services;
+using MySqlSugar;
+
+namespace Business.Services
+{
+    public class DepartmentService : IDepartmentService
+    {
+        public IDepartmentinfoRepository _departmentinfoRepository;
+
+        public DepartmentService(IDepartmentinfoRepository departmentinfoRepository)
+        {
+            _departmentinfoRepository = departmentinfoRepository;
+        }
+
+        /// <summary>
+        /// 获取第一个部门信息
+        /// </summary>
+        /// <returns></returns>
+        public Departmentinfo GetFirst()
+        {
+            return _departmentinfoRepository.Queryable().FirstOrDefault();
+        }
+    }
+}
