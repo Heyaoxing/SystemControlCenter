875db175d4ce3e8e1007a57df47a877be309a824
730530507 <730530507@qq.com>
2016-11-23 20:14:11 +0800

    s

---
+++ SystemControlCenter/Common/Common.Web/PageHelper/MvcAjaxOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc.Ajax;
+
+namespace Common.Web.PageHelper
+{
+    public class MvcAjaxOptions : AjaxOptions
+    {
+        public bool EnablePartialLoading { get; set; }
+        public string DataFormId { get; set; }
+    }
+}
