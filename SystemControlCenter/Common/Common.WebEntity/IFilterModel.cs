4b022d70bd95838f1c3724ea2119a29d669bdebb
730530507 <730530507@qq.com>
2016-11-24 09:24:38 +0800

    s

--- SystemControlCenter/Common/Common.WebEntity/IFilterModel.cs
+++ SystemControlCenter/Common/Common.WebEntity/IFilterModel.cs
@@ -9,11 +9,17 @@ namespace Common.WebEntity
     /// <summary>
     /// 搜索参数实体接口
     /// </summary>
-    public abstract  class IFilterModel
+    public abstract class IFilterModel
     {
         /// <summary>
         /// 当前页数
         /// </summary>
-        public int PageIndex { set; get; } = 1;
+        public int PageIndex
+        {
+            set { this._pageIndex = value; }
+            get { return this._pageIndex; }
+        }
+
+        private int _pageIndex = 1;
     }
 }
